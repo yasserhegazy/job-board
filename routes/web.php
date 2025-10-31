56aac4f9c3f1430e87559fad0f26120142461c8a
Yasser Hegazy <yasseranahegazy@gmail.com>
2025-10-31 14:38:30 +0200

    Creating the jobs view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    redirect('jobs');
 });
+
+Route::resource('jobs', JobController::class)->only(['index']);
+
+
