7d288a56634fe7eaf4ddb720cd9f37de83cec274
Yasser Hegazy <yasseranahegazy@gmail.com>
2025-11-01 11:26:37 +0200

    Add Gradient Background

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
 
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @else
             <style>
@@ -20,7 +21,7 @@
         @endif
         {{-- @vite('resources/css/app.css') --}}
     </head>
-    <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+    <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90% text-slate-700">
         {{ $slot }}
     </body>
 </html>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -38,7 +38,7 @@
                 </div>
 
             </div>
-            <button class="w-full">Filter</button>
+            <x-button class="w-full">Filter</x-button>
         </form>
     </x-card>
     @foreach ($jobs as $job)
