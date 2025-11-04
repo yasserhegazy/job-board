07119e0d57529ed729d126b63a46cc64542a9cba
Yasser Hegazy <yasseranahegazy@gmail.com>
2025-11-04 10:30:49 +0200

    Make the label as a component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -52,9 +52,15 @@
         </nav>
 
         @if (session('success'))
-            <div class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
+            <div role="alert" class="my-8 rounded-md border-l-4 border-green-300 bg-green-100 p-4 text-green-700 opacity-75">
                 <p class="font-bold">Success!</p>
                 <p>{{ session('success') }}</p>
+            </div>
+        @endif
+        @if (session('error'))
+            <div role="alert" class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+                <p class="font-bold">Error!</p>
+                <p>{{ session('error') }}</p>
             </div>
         @endif
         {{ $slot }}
